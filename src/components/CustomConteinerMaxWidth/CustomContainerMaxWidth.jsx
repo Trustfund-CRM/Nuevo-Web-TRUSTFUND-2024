48347bbbe7ae3d50ed3216467e78d9a7140b2802
Pablo Manolaki <86750797+manolonolo@users.noreply.github.com>
2023-12-19 19:27:45 -0300

    css responsive

--- src/components/CustomConteinerMaxWidth/CustomContainerMaxWidth.jsx
+++ src/components/CustomConteinerMaxWidth/CustomContainerMaxWidth.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react"
+
 export const CustomContainerMaxWidth = ({ children }) => {
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        if(window !== 'undefined') {
+            window?.innerWidth < 735 ?
+            setIsMobile(true)
+            : setIsMobile(false)
+        }
+    }, [])
+
     return <div style={{
         display: 'flex',
         flexDirection: 'column',
         width: '100%',
         maxWidth: '1440px',
-        gap: '130px',
+        gap: isMobile ? '130px' : '0px',
     }}>{children}</div>
 }
