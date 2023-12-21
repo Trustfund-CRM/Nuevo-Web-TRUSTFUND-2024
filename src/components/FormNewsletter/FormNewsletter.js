28686b032b1bb68fedc65d4992400124f44b30b9
Pablo Manolaki <86750797+manolonolo@users.noreply.github.com>
2023-12-21 14:00:33 -0300

    FormNewsletter style

--- src/components/FormNewsletter/FormNewsletter.js
+++ src/components/FormNewsletter/FormNewsletter.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { sendContactForm } from "../../../lib/api";
-import style from ".././nuestraGrantia/nuestraGarantia.module.css";
+// import style from ".././nuestraGrantia/nuestraGarantia.module.css";
+import style from './FormNewsletter.module.css'
 import { Accordion, Button, Collapse, Modal } from "react-bootstrap";
 import { CruzModal, modalGarantiza } from "@/styles";
 import Image from "next/image";
@@ -32,94 +33,86 @@ export default function FormNewsletter() {
             className={style.ContainerAccordionInmoYPropie}
             onSubmit={handleSubmit(onSubmit2)}
         >
-            <Accordion
-                style={{ backgroundColor: "transparent !important" }}
-                className={style.AccordionInmoYPropie}
-            >
-                <Accordion.Item className={style.AccordionItem} eventKey="0">
-                    <Accordion.Header className={style.AccordionHeader}>
-                        Garantizá tu propiedad
-                    </Accordion.Header>
-                    <Accordion.Body className={style.AccordionBodyInmoYPropie}>
-                        <div className={style.BoxSelectInmoYPropie}>
-                            <div className="input-wrapper">
-                                <label
-                                    htmlFor="nombre-garantiza"
-                                    className="label-accordion"
-                                    style={{ width: "max-content" }}
-                                >
-                                    Nombre/s
-                                </label>
-                                <input
-                                    className="input-accordion"
-                                    type="text"
-                                    name="Nombre"
-                                    {...register("Nombre", { required: "Campo Requerido" })}
-                                />
-                                {errors.Nombre && (
-                                    <p className="text-danger">{errors.Nombre.message}</p>
-                                )}
-                            </div>
-                            <div className="input-wrapper">
-                                <label
-                                    htmlFor="apellido-garantiza"
-                                    className="label-accordion"
-                                    style={{ width: "max-content" }}
-                                >
-                                    Apellido/s
-                                </label>
-                                <input
-                                    className="input-accordion"
-                                    type="text"
-                                    name="Apellido"
-                                    {...register("Apellido", { required: "Campo Requerido" })}
-                                />
-                                {errors.Apellido && (
-                                    <p className="text-danger">{errors.Apellido.message}</p>
-                                )}
-                            </div>
+            <div className={style.AccordionItem} eventKey="0">
+                <div className={style.AccordionHeader}>
+                    Garantizá tu propiedad
+                </div>
+                <div className={style.AccordionBodyInmoYPropie}>
+                    <div className={style.BoxSelectInmoYPropie}>
+                        <div className={style.inputWrapper} >
+                            <label
+                                htmlFor="nombre-garantiza"
+                                className={style.labelAccordion}
+                            >
+                                Nombre/s
+                            </label>
+                            <input
+                                className={style.inputAccordion}
+                                type="text"
+                                name="Nombre"
+                                {...register("Nombre", { required: "Campo Requerido" })}
+                            />
+                            {errors.Nombre && (
+                                <p className="text-danger">{errors.Nombre.message}</p>
+                            )}
                         </div>
-                        <div className={style.BoxSelectInmoYPropie}>
-                            <div className="input-wrapper">
-                                <label
-                                    htmlFor="email-garantiza"
-                                    className="label-accordion"
-                                    style={{ width: "max-content" }}
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    className="input-accordion"
-                                    type="text"
-                                    name="Email"
-                                    {...register("Email", { required: "Campo Requerido" })}
-                                />
-                                {errors.Email && (
-                                    <p className="text-danger">{errors.Email.message}</p>
-                                )}
-                            </div>
-                            <div className="input-wrapper">
-                                <label
-                                    htmlFor="telefono-garantiza"
-                                    className="label-accordion"
-                                    style={{ width: "max-content" }}
-                                >
-                                    Teléfono de contacto
-                                </label>
-                                <input
-                                    className="input-accordion"
-                                    type="number"
-                                    name="Telefono"
-                                    {...register("Telefono", { required: "Campo Requerido" })}
-                                />
-                                {errors.Telefono && (
-                                    <p className="text-danger">{errors.Telefono.message}</p>
-                                )}
-                                <input type="hidden" id="subject" {...register("subject")} />{" "}
-                                {/* Campo oculto para el asunto */}
-                            </div>
+                        <div className={style.inputWrapper}>
+                            <label
+                                htmlFor="apellido-garantiza"
+                                className={style.labelAccordion}
+                            >
+                                Apellido/s
+                            </label>
+                            <input
+                                className={style.inputAccordion}
+                                type="text"
+                                name="Apellido"
+                                {...register("Apellido", { required: "Campo Requerido" })}
+                            />
+                            {errors.Apellido && (
+                                <p className="text-danger">{errors.Apellido.message}</p>
+                            )}
                         </div>
-                        {/* <div className={style.BoxSelectProvinciaYLocalidad}>
+                    </div>
+                    <div className={style.BoxSelectInmoYPropie}>
+                        <div className={style.inputWrapper}>
+                            <label
+                                htmlFor="email-garantiza"
+                                className={style.labelAccordion}
+                            >
+                                Email
+                            </label>
+                            <input
+                                className={style.inputAccordion}
+                                type="text"
+                                name="Email"
+                                {...register("Email", { required: "Campo Requerido" })}
+                            />
+                            {errors.Email && (
+                                <p className="text-danger">{errors.Email.message}</p>
+                            )}
+                        </div>
+                        <div className={style.inputWrapper}>
+                            <label
+                                htmlFor="telefono-garantiza"
+                                className={style.labelAccordion}
+                            >
+                                Teléfono de contacto
+                            </label>
+                            <input
+                                className={style.inputAccordion}
+                                type="number"
+                                name="Telefono"
+                                {...register("Telefono", { required: "Campo Requerido" })}
+                            />
+                            {errors.Telefono && (
+                                <p className="text-danger">{errors.Telefono.message}</p>
+                            )}
+                            <input type="hidden" id="subject" {...register("subject")} />{" "}
+                            {/* Campo oculto para el asunto */}
+                        </div>
+                    </div>
+                    {/* <div className={style.BoxSelectProvinciaYLocalidad}>
                                 <div className="input-wrapper">
                                     <label
                                         htmlFor="provincia-garantiza"
@@ -166,14 +159,13 @@ export default function FormNewsletter() {
                                     )}
                                 </div>
                             </div> */}
-                        <div className={style.BoxButtonEnd}>
-                            <button type="submit" className={style.ButtonBuscar}>
-                                ENVIAR
-                            </button>
-                        </div>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+                    <div className={style.BoxButtonEnd}>
+                        <button type="submit" className={style.ButtonBuscar}>
+                            ENVIAR
+                        </button>
+                    </div>
+                </div>
+            </div>
             {/* <MyVerticallyCenteredModal
                     show={modalShow}
                     onHide={() => setModalShow(false)}
