20ccea3caf206793f3c3cc04904bd3e425b9e356
Pablo Manolaki <86750797+manolonolo@users.noreply.github.com>
2023-12-28 19:03:15 -0300

    Firma

--- src/components/Servicios/Firma/Firma.js
+++ src/components/Servicios/Firma/Firma.js
@@ -42,7 +42,44 @@ export default function QuienesSomos() {
 
 
                 <div className={style.ContainerBody}>
-                    
+                    <div className={style.ContainerSubTexts}>
+                        <div className={style.subTextLeft}>Gestionamos la firma electrónica de tus documentos. </div>
+                        <div className={style.subTextRight}>Simplificá los procesos de firma, ahorrá tiempo y digitalizate con Trust Fund. </div>
+                    </div>
+
+                    <div className={style.ContainerProceso}>
+                        <div className={style.TitleProceso}>¿Cómo es el proceso?</div>
+
+                        <div className={style.lineaitems}></div>
+
+                        <div className={style.ContainerPasos}>
+
+                            <div className={style.CardPaso}>
+                                <div className={style.numeroPaso}>1</div>
+                                <div className={style.textPaso}>Nos compartís en formato PDF el documento que necesitás que sea firmado electrónicamente.</div>
+                            </div>
+
+                            <div className={style.CardPaso}>
+                                <div className={style.numeroPaso}>2</div>
+                                <div className={style.textPaso}>Desde Trust Fund te generamos links individuales por cada persona firmante.</div>
+                            </div>
+
+                            <div className={style.CardPaso}>
+                                <div className={style.numeroPaso}>3</div>
+                                <div className={style.textPaso}>Los firmantes concretan su firma, adjuntando foto del frente y dorso del DNI y una selfie para validar su identidad.</div>
+                            </div>
+                            
+                            <div className={style.CardPaso}>
+                                <div className={style.numeroPaso}>4</div>
+                                <div className={style.textPaso}>Finalizado el proceso, te compartimos el documento firmado, encriptado y validado por ZapSign. </div>
+                            </div>
+
+                        </div>
+
+                        <div className={style.DisclaimerFirma}>La firma electrónica tiene la misma validez que la firma tradicional.</div>
+
+                        
+                    </div>
                 </div>
             </div>
         </div>
