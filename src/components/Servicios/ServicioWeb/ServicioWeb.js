2ce6eac7bdbcaf831bc2990cd39dee4c4a00df13
Pablo Manolaki <86750797+manolonolo@users.noreply.github.com>
2023-12-29 18:33:12 -0300

    contenido ServicioWeb

--- src/components/Servicios/ServicioWeb/ServicioWeb.js
+++ src/components/Servicios/ServicioWeb/ServicioWeb.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { ButtonSlider } from "@/components/ButtonSlider/ButtonSlider";
 import { useWindowWidth, useWindowHeight } from "@react-hook/window-size";
 import {
     FlechaSolicita,
-    casaPreguntas,
-    serviciosPreguntas,
-    dañosPreguntas,
-    costosPreguntas,
+    imgTablet,
+    starBeneficio,
+    monitorBeneficio,
+    codeBeneficio,
+    globalBeneficio,
+    searchBeneficio,
+    buildingBeneficio,
+    VectorAzul,
+    logoFlexy,
 } from "@/styles";
 import style from './ServicioWeb.module.css';
 
@@ -40,8 +46,8 @@ export default function ServicioWeb() {
 
     return (
         <div>
-            <div className={style.ContainerQuienesSomos}>
-                <div className={style.HeaderQuienesSomos}>
+            <div className={style.ContainerServicioWeb}>
+                <div className={style.HeaderServicioWeb}>
                     <div className={style.ContainerTitle}>
                         <div className={style.preTitle}>Sitio Web</div>
                         <div className={style.Title}>Creamos la página web
@@ -50,11 +56,82 @@ export default function ServicioWeb() {
                 </div>
 
 
-                <div className={style.ContainerBody}>
-                    <div className={style.ContainerBlue}>
-                        
+                <div className={style.ContainerBlue}>
+                    <div className={style.ContainerSubTexts}>
+                        <div className={style.subTextLeft}>Obtenela sin costos de programación ni diseño y
+                            a sólo <span className={style.greenText}>$20.000 mensuales.</span>
+                        </div>
+                        <div className={style.subTextRight}>Potenciá el éxito de tu inmobiliaria con una página web propia, junto a un diseño atractivo y funcional.</div>
+
+                        <ButtonSlider
+                            text={"Ver más"}
+                            customBackground={{ background: "#E6EAEE", color: "#004993" }}
+                            route={"/ConsultasYReclamos"}
+                            suscribe={false}
+                        />
                     </div>
                 </div>
+
+                <Image className={style.imagenTablet} src={imgTablet} />
+
+                <div className={style.Beneficios}>Beneficios de hacer tu sitio web con Trust Fund:</div>
+
+                <div className={style.ContainerCardsBeneficios}>
+
+                    <div className={style.CardBeneficio}>
+                        <Image src={starBeneficio} />
+                        <div className={style.TitleBeneficio}>Diseño moderno</div>
+                        <div className={style.DescBeneficio}>Fácil navegación para tus clientes.</div>
+                    </div>
+                    <div className={style.CardBeneficio}>
+                        <Image src={monitorBeneficio} />
+                        <div className={style.TitleBeneficio}>Diseño responsive</div>
+                        <div className={style.DescBeneficio}>Adaptada a todos los dispositivos.</div>
+                    </div>
+                    <div className={style.CardBeneficio}>
+                        <Image src={codeBeneficio} />
+                        <div className={style.TitleBeneficio}>Fácil navegación</div>
+                        <div className={style.DescBeneficio}>Experiencia personalizada.</div>
+                    </div>
+                    <div className={style.CardBeneficio}>
+                        <Image src={globalBeneficio} />
+                        <div className={style.TitleBeneficio}>Motores de búsqueda</div>
+                        <div className={style.DescBeneficio}>Optimizamos el posicionamiento web.</div>
+                    </div>
+                    <div className={style.CardBeneficio}>
+                        <Image src={searchBeneficio} />
+                        <div className={style.TitleBeneficio}>Búsqueda inteligente</div>
+                        <div className={style.DescBeneficio}>Una búsqueda fácil para tus clientes.</div>
+                    </div>
+                    <div className={style.CardBeneficio}>
+                        <Image src={buildingBeneficio} />
+                        <div className={style.TitleBeneficio}>Cualidades del inmueble</div>
+                        <div className={style.DescBeneficio}>Descripciones, fotos, videos y más.</div>
+                    </div>
+
+                </div>
+
+                <div className={style.ContainerVioleta}>
+                    <div className={style.subContainerVioleta}>
+                        <div className={style.textFlexy}>Las propiedades que subas aparecerán automáticamente en la plataforma de nuestro partner Flexy, llegando así a más personas.</div>
+
+                        <Image src={logoFlexy} />
+                    </div>
+
+                    <Image src={VectorAzul} className={style.VectorAzul} />
+
+                </div>
+
+                <div className={style.ContainerDuda}>
+                    <div className={style.textDuda}>¿Tenés alguna duda?</div>
+
+                    <ButtonSlider
+                        text={""}
+                        customBackground={{ background: "#004993", color: "#F9FAFB" }}
+                        suscribe={false}
+                    />
+                </div>
+
             </div>
         </div>
     )
